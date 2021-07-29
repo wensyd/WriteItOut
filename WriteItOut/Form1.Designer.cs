fa42600fc72731786177a42c7632977a6ec9d560
Wensy DeSousa <wensys013@gmail.com>
2021-07-29 16:27:13 -0700

    Add project files.

---
+++ WriteItOut/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace WriteItOut
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.UsernameLbl = new System.Windows.Forms.Label();
+            this.PasswordLbl = new System.Windows.Forms.Label();
+            this.CreateNewAccountBtn = new System.Windows.Forms.Button();
+            this.SignInBtn = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PasswordField = new System.Windows.Forms.TextBox();
+            this.UsernameField = new System.Windows.Forms.TextBox();
+            this.logo = new System.Windows.Forms.PictureBox();
+            this.CloseBtn = new System.Windows.Forms.Button();
+            this.MinimizeBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // UsernameLbl
+            // 
+            this.UsernameLbl.AutoSize = true;
+            this.UsernameLbl.Location = new System.Drawing.Point(76, 134);
+            this.UsernameLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.UsernameLbl.Name = "UsernameLbl";
+            this.UsernameLbl.Size = new System.Drawing.Size(77, 17);
+            this.UsernameLbl.TabIndex = 21;
+            this.UsernameLbl.Text = "Username:";
+            // 
+            // PasswordLbl
+            // 
+            this.PasswordLbl.AutoSize = true;
+            this.PasswordLbl.Location = new System.Drawing.Point(76, 211);
+            this.PasswordLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.PasswordLbl.Name = "PasswordLbl";
+            this.PasswordLbl.Size = new System.Drawing.Size(73, 17);
+            this.PasswordLbl.TabIndex = 20;
+            this.PasswordLbl.Text = "Password:";
+            // 
+            // CreateNewAccountBtn
+            // 
+            this.CreateNewAccountBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.CreateNewAccountBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CreateNewAccountBtn.Location = new System.Drawing.Point(76, 348);
+            this.CreateNewAccountBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.CreateNewAccountBtn.Name = "CreateNewAccountBtn";
+            this.CreateNewAccountBtn.Size = new System.Drawing.Size(267, 38);
+            this.CreateNewAccountBtn.TabIndex = 19;
+            this.CreateNewAccountBtn.Text = "Create New Account";
+            this.CreateNewAccountBtn.UseVisualStyleBackColor = false;
+            // 
+            // SignInBtn
+            // 
+            this.SignInBtn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(61)))), ((int)(((byte)(96)))));
+            this.SignInBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SignInBtn.ForeColor = System.Drawing.Color.White;
+            this.SignInBtn.Location = new System.Drawing.Point(76, 302);
+            this.SignInBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.SignInBtn.Name = "SignInBtn";
+            this.SignInBtn.Size = new System.Drawing.Size(267, 38);
+            this.SignInBtn.TabIndex = 18;
+            this.SignInBtn.Text = "Sign In";
+            this.SignInBtn.UseVisualStyleBackColor = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel2.Location = new System.Drawing.Point(76, 260);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(267, 1);
+            this.panel2.TabIndex = 17;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(76, 183);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 1);
+            this.panel1.TabIndex = 16;
+            // 
+            // PasswordField
+            // 
+            this.PasswordField.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(172)))), ((int)(((byte)(175)))));
+            this.PasswordField.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PasswordField.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordField.Location = new System.Drawing.Point(76, 234);
+            this.PasswordField.Margin = new System.Windows.Forms.Padding(4);
+            this.PasswordField.Name = "PasswordField";
+            this.PasswordField.Size = new System.Drawing.Size(321, 19);
+            this.PasswordField.TabIndex = 15;
+            this.PasswordField.UseSystemPasswordChar = true;
+            // 
+            // UsernameField
+            // 
+            this.UsernameField.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(172)))), ((int)(((byte)(175)))));
+            this.UsernameField.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.UsernameField.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UsernameField.Location = new System.Drawing.Point(76, 157);
+            this.UsernameField.Margin = new System.Windows.Forms.Padding(4);
+            this.UsernameField.Name = "UsernameField";
+            this.UsernameField.Size = new System.Drawing.Size(321, 19);
+            this.UsernameField.TabIndex = 14;
+            // 
+            // logo
+            // 
+            this.logo.BackColor = System.Drawing.Color.White;
+            this.logo.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("logo.BackgroundImage")));
+            this.logo.Location = new System.Drawing.Point(13, 13);
+            this.logo.Margin = new System.Windows.Forms.Padding(4);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(67, 64);
+            this.logo.TabIndex = 22;
+            this.logo.TabStop = false;
+            // 
+            // CloseBtn
+            // 
+            this.CloseBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseBtn.Location = new System.Drawing.Point(380, 13);
+            this.CloseBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.CloseBtn.Name = "CloseBtn";
+            this.CloseBtn.Size = new System.Drawing.Size(28, 38);
+            this.CloseBtn.TabIndex = 24;
+            this.CloseBtn.UseVisualStyleBackColor = true;
+            // 
+            // MinimizeBtn
+            // 
+            this.MinimizeBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MinimizeBtn.Location = new System.Drawing.Point(350, 13);
+            this.MinimizeBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimizeBtn.Name = "MinimizeBtn";
+            this.MinimizeBtn.Size = new System.Drawing.Size(28, 38);
+            this.MinimizeBtn.TabIndex = 23;
+            this.MinimizeBtn.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(218)))), ((int)(((byte)(172)))), ((int)(((byte)(175)))));
+            this.ClientSize = new System.Drawing.Size(419, 524);
+            this.Controls.Add(this.CloseBtn);
+            this.Controls.Add(this.MinimizeBtn);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.UsernameLbl);
+            this.Controls.Add(this.PasswordLbl);
+            this.Controls.Add(this.CreateNewAccountBtn);
+            this.Controls.Add(this.SignInBtn);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.PasswordField);
+            this.Controls.Add(this.UsernameField);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login ";
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label UsernameLbl;
+        private System.Windows.Forms.Label PasswordLbl;
+        private System.Windows.Forms.Button CreateNewAccountBtn;
+        private System.Windows.Forms.Button SignInBtn;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox PasswordField;
+        private System.Windows.Forms.TextBox UsernameField;
+        private System.Windows.Forms.PictureBox logo;
+        private System.Windows.Forms.Button CloseBtn;
+        private System.Windows.Forms.Button MinimizeBtn;
+    }
+}
+
